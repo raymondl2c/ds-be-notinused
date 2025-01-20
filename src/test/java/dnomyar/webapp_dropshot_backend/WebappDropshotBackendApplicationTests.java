1b89383a45f0c3c7ebf472d288dcf27516f05749
Raymond <raymondllc0408@outlook.com>
2025-01-20 23:24:19 +0800

    Init project

---
+++ src/test/java/dnomyar/webapp_dropshot_backend/WebappDropshotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dnomyar.webapp_dropshot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappDropshotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
